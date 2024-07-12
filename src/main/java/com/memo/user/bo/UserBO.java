52e17e688a69a23bb0507c8e1011fe03e7f35c4f
DESKTOP-K3GM7KB\user <test@test.com>
2024-07-12 18:55:10 +0900

    MEMO 회원가입

---
+++ src/main/java/com/memo/user/bo/UserBO.java
@@ -0,0 +1,31 @@
+package com.memo.user.bo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.memo.user.entity.UserEntity;
+import com.memo.user.repository.UserRepository;
+
+@Service
+public class UserBO {
+	
+	@Autowired
+	UserRepository userRepository;
+	
+	// input: loginId, output: UserEntity/null
+	public UserEntity getUserEntityByLoginId(String loginId) {
+		return userRepository.findByLoginId(loginId);
+	}
+	
+	// input: 4params, output: UserEntity/null
+	public UserEntity addUser(String loginId, String password, String name, String email) {
+		// JPA는 save-builder로 entity로 넘긴다.
+		return userRepository.save(UserEntity.builder()
+				.loginId(loginId)
+				.password(password)
+				.name(name)
+				.email(email)
+				.build());
+		// save 후 저장한 entity를 돌려줌
+	}
+}
